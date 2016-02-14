6b9e91accade864b15c6561fa80aa587c56e5981
Christian Stocker <me@chregu.tv>
2016-02-14 09:51:50 +0100

    make it compile with 5.3

--- php-apache-5.3/build-php-5.4.sh
+++ php-apache-5.3/build-php-5.4.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
+set -e
 cd /root
 
 wget -O php-5.3.29.tar.bz2 http://ch1.php.net/get/php-5.3.29.tar.bz2/from/this/mirror
 tar -xjf php-5.3.29.tar.bz2
 cd php-5.3.29
+
+
+
 ./configure --prefix=/usr/local \
     --enable-bcmath \
     --with-curl \
@@ -17,6 +20,12 @@ cd php-5.3.29
     --with-apxs2 \
     --with-config-file-path=/etc/php5 \
     --enable-intl
+
+# php 5.3 on 64bit
+# from https://github.com/phpbrew/phpbrew/commit/18ef766d0e013ee87ac7d86e338ebec89fbeb445
+sed -i '/^BUILD_/ s/\$(CC)/\$(CXX)/g' Makefile
+sed -i '/EXTRA_LIBS = /s|$| -lstdc++|' Makefile
+
     
 make 
 
@@ -28,7 +37,7 @@ cp ./php.ini-production /etc/php5/php.ini
 pecl install ZendOpcache
 
 cd /root
-rm -rf php-5.3.29*
+#rm -rf php-5.3.29*
 
 rm /etc/apache2/mods-enabled/mpm_event.load
 cd /etc/apache2/mods-enabled/ 
